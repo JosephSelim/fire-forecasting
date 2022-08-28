dd857fdc897a0220d1ac2a3bb789a5529651958c
Joseph Medhat <Joseph.M.Selim@hotmail.com>
2022-08-28 19:20:11 +0200

    edited calculations functions a demo for reporting average and accumulative

--- client/calculations.h
+++ client/calculations.h
@@ -11,19 +11,10 @@ private:
 	double accumulatedTemperatureInCelsius;
 public:
 	Calculations();
-	//Calculations(double temperatureInCelsius, double totalNumberOfRecievedTemperatureValues, double accumulatedTemperatureInCelsius);
 	double calculateAccumulatedTemperatureInCelsius();
 	double calculateAverageTemperatureInCelsius();
 	double getTemperatureInCelsius();
 	void setTemperatureInCelsius(double temperatureInCelsius);
-	//double getAverageTemperatureInCelsius();
-	//double getTotalNumberOfRecievedTemperatureValues();
-	//double getAccumulatedTemperatureInCelsius();
-
-	//void setAccumulatedTemperatureInCelsius(double accumulatedTemperatureInCelsius);
-	//void setAverageTemperatureInCelsius(double averageTemp);
-	//void setTotalNumberOfRecievedTemperatureValues(double totalNumberOfRecievedTemperatureValues);
-
 	void reportAverageAndAccumulative();
 };
 #endif // !CALCULATIONS_H
